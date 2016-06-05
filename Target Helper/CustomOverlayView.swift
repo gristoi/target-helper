9164b75a38f27afd9bc3a0a0512b4ed53bcad9e0
Ian Gristock <iangristock@gmail.com>
2016-06-05 11:00:07 +0100

    comitting calculation

--- Target Helper/CustomOverlayView.swift
+++ Target Helper/CustomOverlayView.swift
@@ -17,25 +17,61 @@ protocol CustomOverlayDelegate{
 class CustomOverlayView: UIView {
     
     let manager = CMMotionManager()
+    var motionLastYaw : Double! = 0
     
+    func degrees(radians:Double) -> Double {
+        return 180 / M_PI * radians
+    }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
         if manager.deviceMotionAvailable {
-            manager.deviceMotionUpdateInterval = 1
+            manager.deviceMotionUpdateInterval = 0.2
             manager.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue()) {
-                (data: CMDeviceMotion?, error: NSError?) in
-                    if let gravity = data?.gravity {
-                        let angle = atan2(data!.gravity.x, data!.gravity.y) + M_PI_2;           // in radians
-                        let angleDegrees = angle * 180.0 / M_PI;   // in degrees
-                        
-                        
-                        
-                        let r = sqrt(data!.gravity.x * data!.gravity.x + data!.gravity.y * data!.gravity.y + data!.gravity.z * data!.gravity.z);
-                        let tiltForwardBackward = acos(data!.gravity.z / r) * 180.0 / M_PI - 90.0;
-                        let result = 1.82 / tan(tiltForwardBackward)
-                        self.cameraLabel.text = "\(result)"
-                    }
+                (deviceMotion: CMDeviceMotion?, error: NSError?) in
+                
+                var attitude = deviceMotion!.attitude
+                var roll = self.degrees(attitude.roll)
+                var pitch = self.degrees(attitude.pitch)
+                var yaw = self.degrees(attitude.yaw)
+                print("Roll: \(roll), Pitch: \(pitch), Yaw: \(yaw)")
+                let result = 1.8 * tan((M_PI * pitch)/180)
+                self.cameraLabel.text = "\(result)"
+//                
+//                let angle = (180/M_PI)*(data?.attitude.pitch)!
+//                let distance = 1.8 / tan(angle)
+//                
+//                let quat = data!.attitude.quaternion;
+//                let yaw = asin(2*(quat.x*quat.z - quat.w*quat.y));
+//                
+//                if (self.motionLastYaw == 0) {
+//                    self.motionLastYaw = yaw;
+//                }
+//                
+//                // kalman filtering
+//                var  q = 0.1;   // process noise
+//                var  r = 0.1;   // sensor noise
+//                var  p = 0.1;   // estimated error
+//                var  k = 0.5;   // kalman filter gain
+//                
+//                var x = self.motionLastYaw;
+//                p = p + q;
+//                k = p / (p + r);
+//                x = x + k*(yaw - x);
+//                p = (1 - k)*p;
+//                self.motionLastYaw = x;
+//                self.cameraLabel.text = "\(self.motionLastYaw)"
+//                    if let gravity = data?.gravity {
+//                        let angle = atan2(data!.gravity.x, data!.gravity.y) + M_PI_2;           // in radians
+//                        let angleDegrees = angle * 180.0 / M_PI;   // in degrees
+//                        
+//                        
+//                        
+//                        let r = sqrt(data!.gravity.x * data!.gravity.x + data!.gravity.y * data!.gravity.y + data!.gravity.z * data!.gravity.z);
+//                        let tiltForwardBackward = acos(data!.gravity.z / r) * 180.0 / M_PI - 90.0;
+//                        let result = 1.82 / tan(tiltForwardBackward)
+//                        self.cameraLabel.text = "\(result)"
+//                    }
                 }
             
         }
